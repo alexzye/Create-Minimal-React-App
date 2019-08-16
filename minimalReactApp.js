82e5c46794418a69faac59d8d2c66b2698246410
Alex Ye <alexzye1@gmail.com>
2019-08-15 23:10:53 -0700

    Add base assets

--- minimalReactApp.js
+++ minimalReactApp.js
@@ -4,6 +4,8 @@
 'use strict'
 const fs = require('fs')
 const cp = require('child_process')
+const path = require('path');
+const utils = require('./utils')
 
 const [,, ...args] = process.argv
 const projectName = args[0]
@@ -13,8 +15,36 @@ if (args.length < 1) {
     process.exit(1) 
 }
 
-// updates process directory, all commands forward will be within project directory
-cp.exec(`mkdir -p ${projectName} && cd ${projectName} && npm init -f`, 
-	(initErr, initStdout, initStderr) => {
-		
-	})
+// creates project
+cp.exec(`mkdir -p ${projectName} && cd ${projectName} && npm init -y`,
+  (initErr, initStdout, initStderr) => {
+    if (initErr) {
+      // todo: write more professional error messages
+      console.error('you dun goofed')
+      console.error('check permissions probably')
+      return
+    }
+
+    console.log(initStdout)
+    console.log(initStderr)
+  }
+)
+
+// copy src and base dirs to project directory
+utils.copyDirectory('./assets/src', `${projectName}/src`)
+utils.copyDirectory('./assets/base', `${projectName}`)
+
+// runs npm install on project install
+cp.exec(`cd ${projectName} && npm install`,
+  (initErr, initStdout, initStderr) => {
+    if (initErr) {
+      // todo: write more professional error messages
+      console.error('you dun goofed')
+      console.error('check permissions probably')
+      return
+    }
+
+    console.log(initStdout)
+    console.log(initStderr)
+  }
+)
